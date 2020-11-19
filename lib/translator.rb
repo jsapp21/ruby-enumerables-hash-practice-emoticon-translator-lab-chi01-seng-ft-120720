5b420920d291a7286fcfdf837fdbd7e50789a307
James Sapp <jameseisapp@gmail.com>
2020-11-19 17:08:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,10 +7,8 @@ def load_library(hash)
   hash = YAML.load_file("lib/emoticons.yml")
     hash.each do |inner_hash, key|
       binding.pry 
-      inner_hash = {}
     end
-      key.each do ||
-    end 
+     
   
   
  
