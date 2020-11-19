238e7709d6f7f2cd594f116e0961d91d6b3385aa
James Sapp <jameseisapp@gmail.com>
2020-11-19 17:50:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,14 +3,12 @@ require 'yaml'
 require 'pry'
 
 def load_library(hash)
- 
   hash = YAML.load_file("lib/emoticons.yml")
     hash.each do |key, value|
       hash[key] = {}
       hash[key][:english] = value[0]
       hash[key][:japanese] = value[1]
     end
-
 end
 
 def get_japanese_emoticon(hash, emoticon)
