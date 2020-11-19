3c52687d72009375d7ee4da6697c81890d5b525b
James Sapp <jameseisapp@gmail.com>
2020-11-19 17:51:08 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,8 +11,7 @@ def load_library(hash)
     end
 end
 
-def get_japanese_emoticon(hash)
-  hash = YAML.load_file("lib/emoticons.yml")
+def get_japanese_emoticon(hash, emoticon)
 end
 
 def get_english_meaning
